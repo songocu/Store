8c733b1ab70312f92ff607c78c55f73f7843a64f
songocu <songocu_tud@yahoo.com>
2025-01-29 15:26:18 +0200

    Add Repository class, first version

---
+++ src/main/java/com/example/stroretool/store/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.stroretool.store.repository;
+
+
+import com.example.stroretool.store.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategory(String category);
+}
