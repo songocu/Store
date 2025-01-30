9f40d51d0b120560349e681a7c9a3a8a586df970
songocu <songocu_tud@yahoo.com>
2025-01-30 13:43:42 +0200

    Add logs to exception

--- src/main/java/com/example/stroretool/store/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/stroretool/store/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.stroretool.store.exception;
 
+import com.example.stroretool.store.service.ProductService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,11 +16,15 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("message", ex.getMessage());
+
+        logger.error("Exception: ", ex);
 
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
